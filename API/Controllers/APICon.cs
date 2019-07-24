2c383a18e75c7cc54cb72a0510f36a07ba974fce
Renato Gomes <reegomescel@gmail.com>
2019-07-23 23:36:03 -0300

    API

---
+++ API/Controllers/APICon.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class APICon : ControllerBase
+    {
+        // GET api/valuesa
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2", "Batata" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            string batata;
+
+            if (id == 1)
+            {
+                batata = "José";
+                return batata;
+            }
+            if (id == 2)
+            {
+                batata = "Dario";
+                return batata;
+            }
+            if (id == 3)
+            {
+                batata = "Pedro";
+                return batata;
+            }
+
+            return null;
+            //return "value teste";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
